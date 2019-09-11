c50222bc7e09de5a9884ea112672d5a91110dd18
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-09-11 16:25:01 +0800

    Initial project setup

---
+++ src/main/java/Library.java
@@ -0,0 +1,9 @@
+public class Library {
+    public static void main(String[] args){
+        System.out.println("When there is a fire, commit and push.");
+    }
+
+    public boolean someLibraryMethod(){
+        return true;
+    }
+}
