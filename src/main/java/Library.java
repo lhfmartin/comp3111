c190c3919aa3c59a0cebbc7a9a205ce5d13ccf41
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-09-13 12:22:24 +0800

    Finished Lab 2

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,8 +1,29 @@
+import lab2.ex1.Book;
+import lab2.ex2.*;
+
+import java.util.Arrays;
+
 public class Library {
     public static void main(String[] args){
-        Library lib = new Library();
-        if(lib.someLibraryMethod())
-            System.out.println("When there is a fire, commit and push.");
+        final String array[] = {"Basic Java", "Advance Java", "Forget about Java"};
+        Book b = new Book(array);
+        System.out.println("The title of Chaper 1 is " + b.getChapter(1));
+        String anotherArray[] = b.getChapters();
+
+        System.out.println("There are " + anotherArray.length + " chapters.");
+        System.out.println(Arrays.toString(anotherArray));
+
+        Computer a = new MobileComputer();
+        for(int i = 0; i < 10; i++){
+            a.work();
+        }
+
+        Charger c = new Charger();
+        Phone p = new Phone();
+        MobileComputer m = new MobileComputer();
+
+        c.charge(p);
+        c.charge(m);
     }
 
     public boolean someLibraryMethod(){
---
+++ src/main/java/lab2/ex1/Book.java
@@ -0,0 +1,21 @@
+package lab2.ex1;
+
+public class Book {
+    private String chapters[];
+    private static final int DEFAULT_CHAPTERS = 10;
+    public Book(){
+        chapters = new String[DEFAULT_CHAPTERS];
+        for(int i = 0; i < chapters.length; i++){
+            chapters[i] = "Chapter " + i;
+        }
+    }
+    public Book(String[] chapters){
+        this.chapters = chapters;
+    }
+    public String getChapter(int i){
+        return chapters[i];
+    }
+    public String[] getChapters(){
+        return chapters;
+    }
+}
