a1e3340483edd6fea7e353cb3bb5b84ae7ef1f1d
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-09-13 17:31:21 +0800

    Finished Lab 3

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,25 +5,7 @@ import java.util.Arrays;
 
 public class Library {
     public static void main(String[] args){
-        final String array[] = {"Basic Java", "Advance Java", "Forget about Java"};
-        Book b = new Book(array);
-        System.out.println("The title of Chaper 1 is " + b.getChapter(1));
-        String anotherArray[] = b.getChapters();
-
-        System.out.println("There are " + anotherArray.length + " chapters.");
-        System.out.println(Arrays.toString(anotherArray));
-
-        Computer a = new MobileComputer();
-        for(int i = 0; i < 10; i++){
-            a.work();
-        }
-
-        Charger c = new Charger();
-        Phone p = new Phone();
-        MobileComputer m = new MobileComputer();
-
-        c.charge(p);
-        c.charge(m);
+        lab3.ex2.UIApplication.run(args);
     }
 
     public boolean someLibraryMethod(){
